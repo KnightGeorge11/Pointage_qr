fbba1c33b521f222156345d295ec18e6ead6b6eb
KnightGeorge11 <mandatedy2003@gmail.com>
2026-08-10 15:35:49 +0300

    fix: Importer react-native-gesture-handler dans App.tsx (dépendance de @react-navigation/stack)

--- mobile/ScanMobileApp/App.tsx
+++ mobile/ScanMobileApp/App.tsx
@@ -1,5 +1,7 @@
 // App.tsx
 import React from 'react'
+// Import GestureHandler MUST BE BEFORE navigation (React Navigation dependency)
+import 'react-native-gesture-handler'
 
 import { AppProvider } from './src/context/AppContext'
 import AppNavigator from './src/navigation/AppNavigator'
