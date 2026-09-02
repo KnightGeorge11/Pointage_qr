59c56fa6b56cc40aa69833bdfb87c61e7ffce122
KnightGeorge11 <mandatedy2003@gmail.com>
2026-09-02 16:01:10 +0300

    Fix regression test for persisted Site TimeField defaults

--- pointage/tests/test_horaires_regression.py
+++ pointage/tests/test_horaires_regression.py
@@ -36,6 +36,11 @@ class HorairesMetierRegressionTests(TestCase):
 
     def test_defaults_du_modele_site_sont_0800_1200_1300_1700(self):
         site = Site.objects.create(nom="Site defaults", adresse="Test")
+        # Un TimeField fraîchement instancié peut conserver sa valeur de
+        # défaut sous forme de chaîne. Après persistance, Django recharge la
+        # valeur sous forme de datetime.time, qui est le contrat public du
+        # modèle utilisé par le domaine.
+        site.refresh_from_db()
         self.assertEqual(site.heure_ouverture_matin, time(8, 0))
         self.assertEqual(site.heure_fermeture_matin, time(12, 0))
         self.assertEqual(site.heure_ouverture_apres_midi, time(13, 0))
