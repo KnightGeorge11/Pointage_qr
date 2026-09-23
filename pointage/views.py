dfd3278ad55fdc500ad61764cb60b401c737c705
KnightGeorge11 <mandatedy2003@gmail.com>
2026-09-24 00:21:36 +0300

    Utiliser la duree de reference configuree pour les syntheses

--- pointage/views.py
+++ pointage/views.py
@@ -1326,7 +1326,7 @@ def export_resume_excel(request):
                     if apm:
                         h_trav += apm.heures_travaillees or timedelta()
                         h_ret  += apm.retard or timedelta()
-                    h_sup = max(timedelta(), h_trav - timedelta(hours=8))
+                    h_sup = max(timedelta(), h_trav - ConfigurationPointage.get_solo().duree_journee_reference)
 
                     tot_trav   += h_trav
                     tot_retard += h_ret
